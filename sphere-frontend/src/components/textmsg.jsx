a98dd144f109728e076ed4adb014e5b250b89446
aaditya1611 <aadityaraj1611@gmail.com>
2025-08-01 05:54:01 +0530

    store user message in a different module

---
+++ sphere-frontend/src/components/textmsg.jsx
@@ -0,0 +1,127 @@
+const Messages = [
+
+    {
+        id: 1,
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, facere dolorum molestias animi natus incidunt quaerat!",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+
+    {
+        id: 1,
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, facere dolorum molestias animi natus incidunt quaerat!",
+        type: "received"
+    },
+
+    {
+        id: 1,
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, facere dolorum molestias animi natus incidunt quaerat!",
+        type: "received"
+    },
+
+    {
+        id: 2,
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, facere dolorum molestias animi natus incidunt quaerat!?",
+        type: "sent"
+    },
+     {
+        id: 2,
+        msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, facere dolorum molestias animi natus incidunt quaerat!?",
+        type: "sent"
+    },
+    {
+        id: 2,
+        msg: "Hey Alex, How are you??",
+        type: "sent"
+    },
+     {
+        id: 1,
+        msg: "Hii Ron, i am okay, How are you?",
+        type: "received"
+    },
+]
+
+
+export default Messages;
