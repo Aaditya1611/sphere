94cdda49e788ba4aaf04a6c70cf88269a5bf0e10
aaditya1611 <aadityaraj1611@gmail.com>
2025-08-17 23:58:37 +0530

    change UI colors

--- sphere-frontend/src/help/helpme.jsx
+++ sphere-frontend/src/help/helpme.jsx
@@ -6,38 +6,36 @@ const HelpMe = () => {
 
 
     return (
-        <div className="h-screen relative lg:overflow-hidden">
-
-            <img src={BGimg} className="absolute top-0 left-0 w-full h-full object-cover -z-50" alt="main bg image"></img>
+        <div className="h-screen relative lg:overflow-hidden bg-neutral-800">
             <div className="flex flex-row justify-between py-6 max-w-full mx-auto lg:px-20 px-5 items-center">
 
-                <Link to={"/"} className="text-4xl font-semibold">Sphere</Link>
-                <Link to={"/signup"} className="underline text-md">Go to signup page</Link>
+                <Link to={"/"} className="text-4xl font-semibold text-white">Sphere</Link>
+                <Link to={"/signup"} className="underline text-md text-white">Go to signup page</Link>
             </div>
-            <h1 className="text-center text-2xl my-10 text-gray-800">
+            <h1 className="text-center text-2xl my-10 text-neutral-400">
                 Advice and answers from the Sphere team
             </h1>
             <div className="my-10 flex justify-center">
                 <form className="flex flex-row items-center">
                     <label htmlFor="helpbox"></label>
-                    <input className="bg-white w-[55rem] h-[4rem] p-5 rounded-l-xl outline-none"
+                    <input className="bg-neutral-500 w-[55rem] h-[4rem] p-5 rounded-l-xl outline-none"
                         type="text"
                         id="searchbox"
                         placeholder="Search your need here"
                     />
-                    <div className="bg-white h-[4rem] p-5 rounded-r-xl w-[4rem]">
+                    <div className="bg-neutral-500 h-[4rem] p-5 rounded-r-xl w-[4rem]">
                         <Search className=" text-gray-800 cursor-pointer" />
                     </div>
                 </form>
             </div>
             <div className="flex flex-row justify-center items-center">
-                <h2 className="text-lg mr-4 underline">Beginner's Guide</h2>
+                <h2 className="text-lg mr-4 underline text-white">Beginner's Guide</h2>
                 <div className="h-[5px] w-[5px] rounded-full bg-neutral-800"></div>
-                <Link className="text-lg ml-4 underline">Contact Us</Link>
+                <Link className="text-lg ml-4 underline text-white">Contact Us</Link>
             </div>
             <div className="mt-10 flex justify-center">
-                <div className="bg-white w-[90rem] rounded-xl p-10 overflow-auto">
-                    <ol className="list-decimal space-y-6 pl-5">
+                <div className="bg-neutral-700 w-[90rem] rounded-xl p-10 overflow-auto">
+                    <ol className="list-decimal space-y-6 pl-5 text-neutral-300">
                         <li>
                                 <p className="text-left font-semibold text-xl mb-3 ml-2">What is sphere?</p>
                                 <p className="text-lg">Sphre is a chat application,designed for realtime communication between clients</p>
