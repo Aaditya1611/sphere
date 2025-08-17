7909fbcb03ab56dc66049309bde4bd781fb1520e
aaditya1611 <aadityaraj1611@gmail.com>
2025-08-17 23:59:02 +0530

    change UI colors

--- sphere-frontend/src/login/forgotpasswd.jsx
+++ sphere-frontend/src/login/forgotpasswd.jsx
@@ -1,44 +1,44 @@
 import { useState } from "react";
-import BGimg from "../assets/loginpageBG.jpeg";
 
-const ForgotPassword = ({onClose}) => {
+const ForgotPassword = ({ onClose }) => {
 
     const [email, setEmail] = useState("");
 
-   const handleSubmit = () => {
-    console.log("Reset link sent to: ", email);
-    onClose();  // This will now work
-    setEmail("");
-  };
+    const handleSubmit = () => {
+        console.log("Reset link sent to: ", email);
+        onClose();  // This will now work
+        setEmail("");
+    };
 
     return (
-        <div className="flex items-center justify-center transform transition-all duration-500 ease-in-out">
+        <div className="fixed inset-0 h-full w-full">
+            <div className="flex flex-col justify-center items-center h-full">
+                <div className="bg-neutral-700 bg-opacity-40 flex flex-col z-50 h-1/2 max-w-[30rem] rounded-2xl p-10">
+                    <h2 className="text-xl font-bold text-center text-white">Reset Password here</h2>
+                    <div className="flex flex-col justify-center items-center h-full gap-4">
+                        <input
+                            type="email"
+                            placeholder="Enter your email here"
+                            className="w-[21rem] py-2 border rounded-lg mb-4 border-neutral-400 focus:outline-none p-2 text-white"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
 
-            <div className="fixed bg-white bg-opacity-40 flex flex-col z-50 h-[45rem] w-[35rem] rounded-2xl ">
-                <h2 className="text-xl font-semibold mt-10 text-center">Reset Password here</h2>
-                <div className="flex flex-col justify-center items-center h-full gap-4">
-                <input
-                    type="email"
-                    placeholder="Enter your email here"
-                    className="w-[21rem] py-2 border rounded-lg mb-4 focus:outline-none focus:ring-blue-500 p-2 "
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                
-                <div className="flex justify-center gap-2">
-                    <button
-                        onClick={onClose}
-                        className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 cursor-pointer"
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        onClick={handleSubmit}
-                        className="px-4 py-2 bg-blue-300 text-white rounded hover:bg-blue-700 cursor-pointer"
-                    >
-                    Send reset code
-                    </button>
-                </div>
+                        <div className="flex justify-center gap-10">
+                            <button
+                                onClick={onClose}
+                                className="px-4 py-2 bg-neutral-200 text-black rounded hover:bg-neutral-800 hover:text-white duration-300 cursor-pointer"
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                onClick={handleSubmit}
+                                className="px-4 py-2 bg-neutral-800 text-white rounded hover:bg-neutral-200 hover:text-black duration-300 cursor-pointer"
+                            >
+                                Send reset code
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
