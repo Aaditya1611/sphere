8aeb959fc91f7a87adcd6dca7f4eb3762f263f74
aaditya1611 <aadityaraj1611@gmail.com>
2025-10-02 09:29:07 +0530

    remove lombok @Data annotation & switch to using conventional getter & setter method

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatInfo.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatInfo.java
@@ -9,16 +9,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "chats")
 public class ChatInfo {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
 
     private String senderName;
     private String recipientName;
@@ -32,9 +35,81 @@ public class ChatInfo {
     private User recipientId;
 
     private String content;
-
     private LocalDateTime timestamp = LocalDateTime.now();
-
     private String status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+       public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getRecipientName() {
+        return recipientName;
+    }
+
+    public void setRecipientName(String recipientName) {
+        this.recipientName = recipientName;
+    }
+
+    public User getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(User senderId) {
+        this.senderId = senderId;
+    }
+
+    public User getRecipientId() {
+        return recipientId;
+    }
+
+    public void setRecipientId(User recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     
 }
