6052b42522d38cfcaa42086b801fdee04c634633
aaditya1611 <aadityaraj1611@gmail.com>
2025-10-02 09:28:29 +0530

    remove lombok @Data annotation & switch to using conventional getter & setter method

--- sphere-backend/src/main/java/com/chatapp/web/login/User.java
+++ sphere-backend/src/main/java/com/chatapp/web/login/User.java
@@ -6,9 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
-@Data
 @Entity
 @Table(name = "user_list")
 public class User {
@@ -16,10 +14,36 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-
+	
 	@Column(name = "username")
 	private String username;
 
 	@Column(name = "password")
 	private String password;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
 }
