7f6e01195a610bfab3c9bba6e1e3288a0161d3aa
aaditya1611 <aadityaraj1611@gmail.com>
2025-10-02 09:20:42 +0530

    entity file to store & fetch user's app preferences

---
+++ sphere-backend/src/main/java/com/chatapp/web/appSettings/Settings.java
@@ -0,0 +1,65 @@
+package com.chatapp.web.appSettings;
+
+import com.chatapp.web.login.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "user_settings")
+public class Settings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    private String bio;
+
+    @Column(columnDefinition = "jsonb")
+    private String preferences;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(String preferences) {
+        this.preferences = preferences;
+    }
+
+    
+}
+
+
