3ffdb4d2891d79f9d8fbd4cc77a602dbcd2be63d
aaditya1611 <aadityaraj1611@gmail.com>
2025-10-02 09:30:36 +0530

    add a new findByUserId method

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.chatapp.web.friends.Friends;
 import com.chatapp.web.login.User;
 
 @Repository
 public interface ChatRepository extends JpaRepository<ChatInfo, Long> {
 
     List<ChatInfo> findBySenderIdOrRecipientIdOrderByTimestamp(User senderId, User recipientId);
+    List<ChatInfo> findByUserId(User userid);
 } 
