a5b8a5dba4d86efca294637b22d58edc03514967
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-19 00:58:29 +0530

    temporary update to the CORS policy config

--- sphere-backend/src/main/java/com/chatapp/web/SecurityConfig.java
+++ sphere-backend/src/main/java/com/chatapp/web/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/ws/**").permitAll()
