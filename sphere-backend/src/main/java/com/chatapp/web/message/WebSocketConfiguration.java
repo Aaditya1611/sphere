e3ebaf8ea8c12fa95ac6c8046fb80c8ae7db22ef
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-19 01:18:15 +0530

    add new method to enable one to one private chat

--- sphere-backend/src/main/java/com/chatapp/web/message/WebSocketConfiguration.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/WebSocketConfiguration.java
@@ -1,7 +1,14 @@
 package com.chatapp.web.message;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,5 +29,28 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         config.setApplicationDestinationPrefixes("/app");
         config.enableSimpleBroker("/topic", "/queue");
         config.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void  configureClientInboundChannel(ChannelRegistration registration) {
+
+        registration.interceptors(new ChannelInterceptor() {
+
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = 
+                    MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    
+                    String username = accessor.getFirstNativeHeader("username");
+                    if(username != null) {
+                        accessor.setUser(() -> username);
+                        System.out.println("Mapped Websocket session to username: " + username);
+                    }
+                }
+                return message;
+            }
+        }); 
     }
 }
