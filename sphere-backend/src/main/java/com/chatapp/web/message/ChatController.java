e4a09b7c54b2a3759fb75ec3d18f5ec060d43ca1
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-19 01:05:16 +0530

    all the message endpoints now save the data to the backend

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatController.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatController.java
@@ -8,24 +8,30 @@ import org.springframework.stereotype.Controller;
 public class ChatController {
 
     private SimpMessageSendingOperations msgTemplate;
+    private ChatService chatService;
 
-    public ChatController(SimpMessageSendingOperations msgTemplate) {
+    public ChatController(SimpMessageSendingOperations msgTemplate, ChatService chatService) {
         this.msgTemplate = msgTemplate;
+        this.chatService = chatService;
     }
 
     @MessageMapping("/Message")
     public ChatInfo sendMessage(ChatInfo chatMessage) {
 
-        System.out.println("Recieved message from user: " + chatMessage.getSenderName() + ": " + chatMessage.getContent() );
+       // System.out.println("Recieved message from user: " + chatMessage.getSenderName() + ": " + chatMessage.getContent() );
+        chatService.saveChats(chatMessage);
         msgTemplate.convertAndSend("/topic/Message", chatMessage);
-        System.out.println("Sent message to /topic/Message: " + chatMessage.getRecipientName() + ": " + chatMessage.getContent());
+       // System.out.println("Sent message to /topic/Message: " + chatMessage.getRecipientName() + ": " + chatMessage.getContent());
         return chatMessage;
     }
 
     @MessageMapping("/PrivateMessage")
     public ChatInfo sendPrivateMessage(ChatInfo chatMessage) {
 
+        System.out.println("Recieved message from user: " + chatMessage.getSenderName() + ": " + chatMessage.getContent() );
+        chatService.saveChats(chatMessage);
         msgTemplate.convertAndSendToUser( chatMessage.getRecipientName(), "/queue/PrivateMessage", chatMessage);
+        System.out.println("Sent message to /queue/PrivateMessage: " + chatMessage.getRecipientName() + ": " + chatMessage.getContent());
         return chatMessage;
     }
 }
