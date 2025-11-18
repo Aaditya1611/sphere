c975d64737521f1774c49da763484531fc07b0b1
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-19 00:45:13 +0530

    add new methods to send & recieve private message

--- sphere-frontend/src/modules/webSocketService.js
+++ sphere-frontend/src/modules/webSocketService.js
@@ -4,33 +4,73 @@ import SockJS from 'sockjs-client/dist/sockjs';
 
 window.global ||= window;
 let stompClient = null;
+let isConnected = false;
 
-export const connectWebSocket = (onMessageRecevied) => {
+export const connectWebSocket = (userData, PublicMsg, PrivateMsg) => {
+
+    if (isConnected) {
+        console.log("Websocket is already connected. Skipping...");
+        return
+    }
+
     const socket = new SockJS('http://localhost:8080/ws');
     stompClient = new Client({
         webSocketFactory: () => socket,
-        debug: (str) => console.log(str)
+        //debug: (str) => console.log(str),
+        connectHeaders: {
+            username: userData.username
+        }
     });
+    try {
+        stompClient.onConnect = () => {
+            isConnected = true;
+            console.log('Connected to WebSocket')
 
-    stompClient.OnConnect = () => {
-        console.log('Connected to WebSocket');
+            // PUBLIC MESSAGES (BROADCAST)
+            stompClient.subscribe('/topic/Message', (message) => {
+                const body = JSON.parse(message.body);
+                PublicMsg?.(body);
+                console.log("Message received:", body);
+            });
 
-        stompClient.subscribe('/topic/Message', (msg) => {
-            const message = JSON.parse(msg.body);
-            onMessageRecevied(message);
-        });
-    };
-
+            // PRIVATE MESSAGE (BROADCAST)
+            stompClient.subscribe('/user/queue/PrivateMessage', (message) => {
+                const body = JSON.parse(message.body);
+                PrivateMsg?.(body);
+                console.log("Private Message recieved: ", body)
+            });
+        }
+    } catch (error) {
+        console.log("Websocket connection failed", error)
+    }
     stompClient.activate();
-
 };
 
 export const sendMessage = (chatMessage) => {
 
-    if (stompClient && stompClient.connected) {
-        stompClient.publish({
-            destination: "/app/Message",
-            body: JSON.stringify(chatMessage)
-        });
+    try {
+        if (stompClient && stompClient.connected) {
+            stompClient.publish({
+                destination: "/app/Message",
+                body: JSON.stringify(chatMessage)
+            });
+        }
+    } catch (error) {
+        console.log("failed to send message", error)
     }
 };
+
+export const sendPrivateMessage = (chatMessage) => {
+
+    try {
+        console.log("Private message sent: ", chatMessage)
+        if (stompClient && stompClient.connected) {
+            stompClient.publish({
+                destination: "/app/PrivateMessage",
+                body: JSON.stringify(chatMessage)
+            });
+        }
+    } catch (error) {
+        console.log("failed to send message, error")
+    }
+}
