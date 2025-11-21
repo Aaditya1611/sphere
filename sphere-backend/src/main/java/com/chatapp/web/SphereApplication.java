9385d4a2b2e9eb53332abed7ec223cc540024aa0
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-22 01:45:06 +0530

    enable scheduling

--- sphere-backend/src/main/java/com/chatapp/web/SphereApplication.java
+++ sphere-backend/src/main/java/com/chatapp/web/SphereApplication.java
@@ -2,8 +2,10 @@ package com.chatapp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SphereApplication {
 
 	public static void main(String[] args) {
