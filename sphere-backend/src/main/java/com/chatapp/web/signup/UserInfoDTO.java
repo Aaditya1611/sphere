e420402ff9f8b7f5a616a0d8142342258b86d16a
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-22 02:11:14 +0530

    new DTO file for fetching, storing data in db

---
+++ sphere-backend/src/main/java/com/chatapp/web/signup/UserInfoDTO.java
@@ -0,0 +1,12 @@
+package com.chatapp.web.signup;
+
+import lombok.Data;
+
+@Data
+public class UserInfoDTO {
+    
+    private Long userId;
+    private String bio;
+    private String firstname;
+    private String lastname;
+}
