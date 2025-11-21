68b147844f7d55eab8fa4468d29c29157aa2bbc2
aaditya1611 <aadityaraj1611@gmail.com>
2025-11-22 02:03:46 +0530

    new custom query method to delete chats

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
@@ -2,18 +2,30 @@ package com.chatapp.web.message;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface ChatRepository extends JpaRepository<ChatInfo, Long> {
 
     @Query("SELECT c FROM ChatInfo c WHERE " +
-           "((c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
-           "(c.senderId = :recipientId AND c.recipientId = :senderId)) " +
-           "ORDER BY c.timestamp ASC")
+            "((c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
+            "(c.senderId = :recipientId AND c.recipientId = :senderId)) " +
+            "ORDER BY c.timestamp ASC")
     List<ChatInfo> findConversationBetween(@Param("senderId") Long senderId, @Param("recipientId") Long recipientId);
-    
+
     List<ChatInfo> findBySenderIdOrRecipientIdOrderByTimestamp(Long senderId, Long recipientId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ChatInfo c WHERE " +
+            "(c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
+            "(c.senderId = :recipientId AND c.recipientId = :senderId)")
+    void deleteChatsBothSides(
+            @Param("senderId") Long senderId,
+            @Param("recipientId") Long recipientId);
 }
