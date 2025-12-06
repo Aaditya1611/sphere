d6ed43ba32f23f07a8ed47cd53981a70d78dc0cd
aaditya1611 <aadityaraj1611@gmail.com>
2025-12-07 05:17:16 +0530

    new service file for Otp verification

---
+++ sphere-backend/src/main/java/com/chatapp/web/signup/otp/OtpService.java
@@ -0,0 +1,75 @@
+package com.chatapp.web.signup.otp;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OtpService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public OtpRepository otpRepository;
+
+
+    public String generateOTP() {
+
+        return String.valueOf(100000 + new Random().nextInt(999999));
+    }
+
+    public void sendOtpEmail(String email) {
+
+        String otp = generateOTP();
+
+        LocalDateTime expiry = LocalDateTime.now().plusMinutes(5);
+        OtpEntity existing = otpRepository.findByEmail(email).orElse(null);
+
+        if(existing == null) {
+            existing = new OtpEntity();
+            existing.setEmail(email);
+        }
+
+        existing.setOtp(otp);
+        existing.setExpiryTime(expiry);
+        existing.setAttempts(0);
+
+        otpRepository.save(existing);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Here is your verfication code: ");
+        message.setText("Your OTP is " + otp + "\n Otp is valid only for 5 minutes");
+
+        mailSender.send(message);
+    }
+    
+    public boolean verifyOtp(String email, String enteredotp) {
+
+        OtpEntity entity = otpRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Otp not found for this user"));
+
+        if(entity.getExpiryTime().isBefore(LocalDateTime.now())) {
+            otpRepository.delete(entity);
+            throw new RuntimeException("OTP expired");
+        };
+
+        if(entity.getAttempts() >= 3 ) {
+            otpRepository.delete(entity);
+            throw new RuntimeException("Too many attempts");
+        }
+
+        if(!entity.getOtp().equals(enteredotp)) {
+            entity.setAttempts(entity.getAttempts() + 1);
+            otpRepository.save(entity);
+            throw new RuntimeException("Invalid OTP");
+        }
+
+        //otpRepository.delete(entity);
+        return true;
+    }
+}
