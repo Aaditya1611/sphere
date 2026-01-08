c67a967db38039f0acea3ed5a84383ac30130b6d
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-09 04:11:03 +0530

    fixed the broken signup page UI

--- sphere-frontend/src/signup/signupPage.jsx
+++ sphere-frontend/src/signup/signupPage.jsx
@@ -15,6 +15,7 @@ const Signup = () => {
     const [verifyEmail, setVerifyEmail] = useState(false);
     const [signupSuccess, setSignupSuccess] = useState(false);
     const [errorMsg, setErrorMsg] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [promptMsg, setPromptMsg] = useState("");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -55,7 +56,7 @@ const Signup = () => {
             username: formData.username,
             password: formData.password
         }
-        const isSuccess = await signup(payload) 
+        const isSuccess = await signup(payload)
         if (isSuccess) {
             setSignupSuccess(true)
         } else {
@@ -71,6 +72,8 @@ const Signup = () => {
 
     const handleSendOtp = async () => {
 
+        setIsLoading(true);
+
         if (!emailRegex.test(formData.email)) {
             setPromptMsg("The email is not valid")
             return;
@@ -79,7 +82,8 @@ const Signup = () => {
         if (isSuccess) {
             setSendOtp(true);
         } else {
-            setPromptMsg("Sending OTP failed, please try again later")
+            setPromptMsg("Sending OTP failed, please try again later");
+            setIsLoading(false)
         }
     };
 
@@ -89,7 +93,7 @@ const Signup = () => {
             return;
         };
         const isSuccess = await verifyOtp(formData.email, formData.otp)
-        if(isSuccess){
+        if (isSuccess) {
             setVerifyEmail(true)
         } else {
             setPromptMsg("Verifying the OTP failed, please try again later")
@@ -105,55 +109,65 @@ const Signup = () => {
                 <h1 className="text-5xl font-semibold text-white">Sign Up</h1>
                 <p className="text-md mt-3 text-neutral-800">Enter your details to begin</p>
                 <form className="flex flex-col">
-                    <div className="mt-5 flex flex-row items-center gap-4">
-                        <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
-                            required
-                            type="text"
-                            id="email"
-                            name="email"
-                            placeholder="Enter your Email id"
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="mt-3">
-                        <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
-                            required
-                            type="text"
-                            name="otp"
-                            inputMode="numeric"
-                            maxLength={6}
-                            placeholder="Enter your OTP"
-                            value={formData.otp}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="mt-3">
-                        <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
-                            required
-                            type="text"
-                            id="username"
-                            name="username"
-                            placeholder="Create your username"
-                            value={formData.username}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="mt-3">
-                        <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
-                            required
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Create a password"
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
-                    </div>
+                    {!verifyEmail && (
+
+                        <div>
+                            <div className="mt-5 flex flex-row items-center gap-4">
+                                <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
+                                    required
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    placeholder="Enter your Email id"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="mt-3">
+                                <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
+                                    required
+                                    type="text"
+                                    name="otp"
+                                    inputMode="numeric"
+                                    maxLength={6}
+                                    placeholder="Enter your OTP"
+                                    value={formData.otp}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                    )}
+                    {verifyEmail && (
+
+                        <div>
+                            <div className="mt-3">
+                                <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
+                                    required
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    placeholder="Create your username"
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="mt-3">
+                                <input className="bg-neutral-400 w-[25rem] h-[3rem] rounded-full p-5 border-none focus:outline-none"
+                                    required
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    placeholder="Create a password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                    )}
                 </form>
                 {onSendOtp && (
                     <div>
-                        <p className="text-blue-500 relative">{verifyEmail ? "Email verified successfully" : "OTP sent"}</p>
+                        <p className="text-blue-500 relative mt-4">{verifyEmail ? "Email verified successfully" : "OTP sent"}</p>
                     </div>
                 )}
                 {errorMsg && (
@@ -163,16 +177,26 @@ const Signup = () => {
                 <div>
                     <h1 className="text-red-300">{promptMsg}</h1>
                 </div>
+
                 {!onSendOtp && (
                     <button
-                        className="flex flex-row gap-x-2 items-center justify-center px-5 py-3 text-white bg-neutral-700 hover:bg-neutral-200 hover:text-black duration-300 rounded-full mt-5 cursor-pointer"
+                        disabled={isLoading}
+                        className={`flex flex-row gap-x-2 items-center justify-center mt-5 px-5 py-3 duration-300 rounded-full text-white bg-neutral-700
+                            ${isLoading ? "cursor-not-allowed opacity-70" : "hover:text-black hover:bg-neutral-200 cursor-pointer"}
+                            `}
                         onClick={() => { handleSendOtp() }}
                     >
-                        Send OTP
-                        <ArrowRight className="" size={20} />
+                        {isLoading ? (
+                            <div className="w-5 h-5 border-3 border-neutral-white border-t-neutral-900 rounded-full animate-spin"></div>
+                        ) : (
+                            <>
+                                Send OTP
+                                <ArrowRight className="" size={20} />
+                            </>
+                        )}
                     </button>
                 )}
-                {onSendOtp && (
+                {onSendOtp && !verifyEmail && (
                     <button
                         className="flex flex-row gap-x-2 items-center justify-center px-5 py-3 text-white bg-neutral-700 hover:bg-neutral-200 hover:text-black duration-300 rounded-full mt-5 cursor-pointer"
                         onClick={() => { handleVerifyOtp() }}
