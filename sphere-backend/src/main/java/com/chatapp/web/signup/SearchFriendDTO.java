8138f2c028085bb4bdef763685e34440e4b21333
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-09 04:22:13 +0530

    new DTO file for returning only the required data to add friends

---
+++ sphere-backend/src/main/java/com/chatapp/web/signup/SearchFriendDTO.java
@@ -0,0 +1,14 @@
+package com.chatapp.web.signup;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SearchFriendDTO {
+    
+    private Long id;
+    private String username;
+}
