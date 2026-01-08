a072e53ebfc23b2579b237e16c0bef28b0ab194a
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-09 04:14:40 +0530

    update existing & add new functions

--- sphere-backend/src/main/java/com/chatapp/web/friends/FriendController.java
+++ sphere-backend/src/main/java/com/chatapp/web/friends/FriendController.java
@@ -3,62 +3,58 @@ package com.chatapp.web.friends;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import com.chatapp.web.login.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/user/friends")
 public class FriendController {
 
     @Autowired
     private FriendService friendService;
 
-    @PostMapping("/addFriend")
-    public ResponseEntity<?> AddNewFriend(@RequestBody Friends friends) {
-        
-        boolean addedorBlocked = friendService.addFriend(friends);
+    // @GetMapping("/getFriends/{userId}")
+    // public List<Friends> getAllFriends(@PathVariable Long userId) {
 
-        if(!addedorBlocked) {
+    //     return friendService.GetAllFriends(userId);
+    // }
+
+    @GetMapping("/getBlockedUsers/{userId}")
+    public ResponseEntity<?> getBlockedUsers(@PathVariable Long userId) {
+
+        List<BlockedFriendDTO> blockedUsers =  friendService.getAllBlockedUsers(userId);
+        return ResponseEntity.ok(blockedUsers);
+    }
+
+    @PostMapping("/addFriend")
+    public ResponseEntity<?> addNewFriend(@RequestBody Friends friends) {
+
+        boolean addedorBlocked = friendService.addFriend(friends.getUserId(), friends.getFriend());
+
+        if (!addedorBlocked) {
             return ResponseEntity.status(409).body("Friend already added or blocked");
         }
         return ResponseEntity.ok().body("Friend added successfully");
     }
 
     @PostMapping("/blockUser")
-    public ResponseEntity<?> BlockUsers(@RequestBody Friends blockedUser) {
-        
-       boolean blocked = friendService.blockUser(blockedUser);
+    public ResponseEntity<?> blockUsers(@RequestBody Friends blockUser) {
 
-       if(!blocked) {
+        boolean blocked = friendService.blockUser(blockUser.getUserId(), blockUser.getBlockedUser());
+
+        if (!blocked) {
             return ResponseEntity.status(409).body("Something went wrong");
-       }
-       return ResponseEntity.ok().body("User Blocked");
-    }
-    
-    @GetMapping("/getFriends/{userId}")
-    public List<Friends> GetAllFriends(@PathVariable User userId) {
-
-        return friendService.GetAllFriends(userId);
-    }
-
-    @GetMapping("/getBlockedUsers/{userId}")
-    public List<Friends> GetBlockedUsers(@PathVariable Long userId) {
-
-        User user = new User();
-        user.setId(userId);
-        return friendService.GetAllBlockedUsers(user);
+        }
+        return ResponseEntity.ok().body("User Blocked");
     }
 
     @PostMapping("/unblockUser")
-    public ResponseEntity<?> UnBlockUser(@RequestBody Friends friends) {
+    public ResponseEntity<?> unBlockUser(@RequestBody Friends friends) {
 
-        friendService.unBlockUser(friends);
+        friendService.unBlockUser(friends.getUserId(), friends.getBlockedUser());
         return ResponseEntity.ok().body("User removed from blocklist");
     }
-    
+
 }
