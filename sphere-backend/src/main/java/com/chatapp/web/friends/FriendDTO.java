d204fac8c096a504152d85b731b38974501da1c0
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-09 04:15:09 +0530

    minor changes

--- sphere-backend/src/main/java/com/chatapp/web/friends/FriendDTO.java
+++ sphere-backend/src/main/java/com/chatapp/web/friends/FriendDTO.java
@@ -9,13 +9,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class FriendDTO {
     
-    // private Long id;
-    //private Long userId;
+    private Long id;
     private String firstname;
     private String lastname;
     private String bio;
     private String email;
-    private Long friend;
-    private Long blockedUser;
-    private String blockedUserEmail;
 }
