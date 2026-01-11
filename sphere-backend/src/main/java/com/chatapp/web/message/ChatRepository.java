cf0a7ccfeb7162ef81f00c5e109a4601e939cfd7
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-11 07:11:44 +0530

    new method to update msg status in the database

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatRepository.java
@@ -12,20 +12,31 @@ import jakarta.transaction.Transactional;
 @Repository
 public interface ChatRepository extends JpaRepository<ChatInfo, Long> {
 
-    @Query("SELECT c FROM ChatInfo c WHERE " +
-            "((c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
-            "(c.senderId = :recipientId AND c.recipientId = :senderId)) " +
-            "ORDER BY c.timestamp ASC")
-    List<ChatInfo> findConversationBetween(@Param("senderId") Long senderId, @Param("recipientId") Long recipientId);
+        List<ChatInfo> findBySenderIdOrRecipientIdOrderByTimestamp(Long senderId, Long recipientId);
 
-    List<ChatInfo> findBySenderIdOrRecipientIdOrderByTimestamp(Long senderId, Long recipientId);
+        long countBySenderIdAndRecipientIdAndStatus(Long senderId, Long recipientId, MessageStatus status);
 
-    @Transactional
-    @Modifying
-    @Query("DELETE FROM ChatInfo c WHERE " +
-            "(c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
-            "(c.senderId = :recipientId AND c.recipientId = :senderId)")
-    void deleteChatsBothSides(
-            @Param("senderId") Long senderId,
-            @Param("recipientId") Long recipientId);
+        @Query("SELECT c FROM ChatInfo c WHERE " +
+                        "((c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
+                        "(c.senderId = :recipientId AND c.recipientId = :senderId)) " +
+                        "ORDER BY c.timestamp ASC")
+        List<ChatInfo> findConversationBetween(@Param("senderId") Long senderId,
+                        @Param("recipientId") Long recipientId);
+
+        @Transactional
+        @Modifying
+        @Query("DELETE FROM ChatInfo c WHERE " +
+                        "(c.senderId = :senderId AND c.recipientId = :recipientId) OR " +
+                        "(c.senderId = :recipientId AND c.recipientId = :senderId)")
+        void deleteChatsBothSides(
+                        @Param("senderId") Long senderId,
+                        @Param("recipientId") Long recipientId);
+
+        @Transactional
+        @Modifying
+        @Query("UPDATE ChatInfo c SET c.status = 'READ' WHERE c.senderId = :senderId AND c.recipientId = :recipientId")
+        void updateMessageStatusToRead(
+                        @Param("senderId") Long senderId,
+                        @Param("recipientId") Long recipientId);
+
 }
