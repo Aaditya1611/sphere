398b019a211632d61b76be9be1003f817956f07a
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-13 08:59:05 +0530

    fix the markMessages function and remove all unnecessary code

--- sphere-backend/src/main/java/com/chatapp/web/message/ChatController.java
+++ sphere-backend/src/main/java/com/chatapp/web/message/ChatController.java
@@ -16,49 +16,26 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-    @MessageMapping("/Message")
-    public ChatInfo sendMessage(ChatInfo chatMessage) {
+    // @MessageMapping("/Message") // public message endpoint // dont need this right now
+    // public ChatInfo sendMessage(ChatInfo chatMessage) {
 
-        // System.out.println("Recieved message from user: " +
-        // chatMessage.getSenderName() + ": " + chatMessage.getContent() );
-
-        chatService.saveChats(chatMessage);
-        msgTemplate.convertAndSend("/topic/Message", chatMessage);
-
-        // System.out.println("Sent message to /topic/Message: " +
-        // chatMessage.getRecipientName() + ": " + chatMessage.getContent());
-        return chatMessage;
-    }
+    //     chatService.saveChats(chatMessage);
+    //     msgTemplate.convertAndSend("/topic/Message", chatMessage);
+    //     return chatMessage;
+    // }
 
     @MessageMapping("/PrivateMessage")
     public ChatInfo sendPrivateMessage(ChatInfo chatMessage) {
 
-        // System.out.println("Recieved message from user: " +
-        // chatMessage.getSenderName() + ": " + chatMessage.getContent() );
-
         chatService.saveChats(chatMessage); // this is Async method, saving messages to database can happen later on, 
         msgTemplate.convertAndSendToUser(String.valueOf(chatMessage.getRecipientId()), "/queue/PrivateMessage", chatMessage);
-        
-        // System.out.println("Sent message to /queue/PrivateMessage: " +
-        // chatMessage.getRecipientName() + ": " + chatMessage.getContent());
         return chatMessage;
     }
 
-    @MessageMapping("/read-messages")
+    @MessageMapping("/MessageStatus")
     public void markMessagesAsRead(@Payload ReadReceiptRequestDTO request) {
 
-        System.out.println("========== READ RECEIPT REQUEST ==========");
-        System.out.println("📥 Received read receipt request:");
-        System.out.println("the person who read the message and send the receipt: " + request.getRecipientId());
-        System.out.println("the person who needs to check for this receipt: " + request.getSenderId());
-
-        // recipientId = original message sender (whose messages to mark as READ)
-        // senderId = the person reading the messages
         chatService.markMessagesAsRead(request.getSenderId(), request.getRecipientId());
-        
-        // Send receipt back to the original message sender using their firstname
-        msgTemplate.convertAndSendToUser(String.valueOf(request.getSenderId()), "/queue/read-receipt", request);
-        System.out.println("✅ Receipt sent successfully to: " + request.getSenderId());
-        System.out.println("==========================================");
+        msgTemplate.convertAndSendToUser(String.valueOf(request.getSenderId()), "/queue/MessageStatus", request);
     }
 }
