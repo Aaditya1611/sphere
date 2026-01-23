49b2ae22bc3003a4b55d1c8ada0ab8416c7d2256
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-23 22:18:05 +0530

    add new Jwt service file

--- sphere-backend/src/main/java/com/chatapp/web/login/JWTService.java
+++ sphere-backend/src/main/java/com/chatapp/web/login/JWTService.java
@@ -1,9 +1,56 @@
 package com.chatapp.web.login;
 
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Base64.Decoder;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JWTService {
-    
-    
+
+    private String secretKey = "keyWzA21AHKb8NcC7STopUjHeA9SmkoKvi6Q2GIcYxS1Sc";
+
+    // public JWTService() {
+    //     try {
+    //         KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+    //         SecretKey sk = keyGen.generateKey();
+    //         secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+    //         System.out.println("This is the secret key" + secretKey);
+    //     } catch (NoSuchAlgorithmException e) {
+    //         throw new RuntimeException(e);
+    //     }
+    // }
+
+    public String generateToken(String username) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
