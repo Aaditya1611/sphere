8a65db64b77aff7a3a9f9662cd3bc53988b61a3b
aaditya1611 <aadityaraj1611@gmail.com>
2026-01-24 03:25:37 +0530

    add new methods

--- sphere-backend/src/main/java/com/chatapp/web/login/JWTService.java
+++ sphere-backend/src/main/java/com/chatapp/web/login/JWTService.java
@@ -7,12 +7,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Base64.Decoder;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -23,14 +26,14 @@ public class JWTService {
     private String secretKey = "keyWzA21AHKb8NcC7STopUjHeA9SmkoKvi6Q2GIcYxS1Sc";
 
     // public JWTService() {
-    //     try {
-    //         KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-    //         SecretKey sk = keyGen.generateKey();
-    //         secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
-    //         System.out.println("This is the secret key" + secretKey);
-    //     } catch (NoSuchAlgorithmException e) {
-    //         throw new RuntimeException(e);
-    //     }
+    // try {
+    // KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+    // SecretKey sk = keyGen.generateKey();
+    // secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+    // System.out.println("This is the secret key" + secretKey);
+    // } catch (NoSuchAlgorithmException e) {
+    // throw new RuntimeException(e);
+    // }
     // }
 
     public String generateToken(String username) {
@@ -48,9 +51,45 @@ public class JWTService {
                 .compact();
     }
 
-    private Key getKey() {
+    private SecretKey getKey() {
 
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+    
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+
+        return extractClaim(token, Claims::getExpiration);
     }
 }
